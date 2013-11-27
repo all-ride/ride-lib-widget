3b30c8926d17f2c9244bc804115d42ec502f4bcf
Joris Vandeweerd <joris@statik.be>
2013-11-28 00:12:32 +0100

    initial commit

---
+++ src/pallo/library/widget/WidgetModel.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace pallo\library\widget;
+
+/**
+ * Interface for the model of the available widgets
+ */
+interface WidgetModel {
+
+    /**
+     * Gets the instance of a widget
+     * @param string $widget Machine name of the widget
+     * @return pallo\library\widget\Widget
+     */
+    public function getWidget($widget);
+
+    /**
+     * Gets the available widgets
+     * @return array Array with the machine name of the widget as key and an
+     * instance of Widget as value
+     */
+    public function getWidgets();
+
+}
